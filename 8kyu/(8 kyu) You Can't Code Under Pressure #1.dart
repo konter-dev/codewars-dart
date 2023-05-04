67a4bbdfc4e417dec5d9d406fdf9f36116a10354
אדם קונטר <konter.dev@gmail.com>
2023-05-04 07:46:18 +0200

    Update (8 kyu) You Can't Code Under Pressure #1.dart

--- 8kyu/(8 kyu) You Can't Code Under Pressure #1.dart
+++ 8kyu/(8 kyu) You Can't Code Under Pressure #1.dart
@@ -1,3 +1,9 @@
+/**
+* (8 kyu) You Can't Code Under Pressure #1
+* https://www.codewars.com/kata/53ee5429ba190077850011d4
+* Code as fast as you can! You need to double the integer and return it.
+*/
+
 // 1 Plain solution
 int doubleInteger(int i) {
   return i * 2;
